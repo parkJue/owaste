f20a218ade95059eda97ee17370bbc5671ca3d03
parkeu <pej6224@naver.com>
2022-11-15 11:20:28 +0900

    댓글 빼고 진짜 구현 끝.

--- zerowaste/views.py
+++ zerowaste/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Shop
 from .models import Nkreview
 from django.db.models import Q
-
+from .forms import CommentForm
 
 info_list = Shop.objects.all().order_by('id')
 search_list = Shop.objects.all().order_by('id')
@@ -19,16 +19,62 @@ def base(request):
 
 
 def search(request):
-    global info_list
-    global search_list
-    search_key = request.GET.get('search_key')
+
+    info_list = Shop.objects.all().order_by('id')
+    search_list = []
+    search_key = request.GET.get('search_key', '')
     print(search_key)
-    if search_key:
+
+    all = []
+    category_one = request.GET.get('category', '')
+    subject_list = request.GET.getlist('subject', '')
+    facility_list = request.GET.getlist('facility', '')
+
+    q = Q()
+
+    if search_key != '':
         search_list = info_list.filter(
             Q(name__icontains=search_key))  # 이름으로 찾기
         print('search_list', search_list)
+
+        # search_list에서 시설, 추가해서 filter해서
+        if category_one:
+            q.add(Q(category=category_one), q.AND)
+        if subject_list:
+            for i in range(0, len(subject_list)):
+                q.add(Q(subject__icontains=subject_list[i]), q.AND)
+        if facility_list:
+            for i in range(0, len(facility_list)):
+                q.add(Q(facility__icontains=facility_list[i]), q.AND)
+
+        search_list = search_list.filter(q)
+
+    else:
+        search_list = info_list
+
+        # search_list에서 시설, 추가해서 filter해서
+        if category_one:
+            q.add(Q(category=category_one), q.AND)
+        if subject_list:
+            for i in range(0, len(subject_list)):
+                q.add(Q(subject__icontains=subject_list[i]), q.AND)
+        if facility_list:
+            for i in range(0, len(facility_list)):
+                q.add(Q(facility__icontains=facility_list[i]), q.AND)
+
+        search_list = search_list.filter(q)
+
+    subject = ','.join(subject_list)
+    facility = ','.join(facility_list)
+
+    print(subject)
+
     # search_list들을 info_list라는 이름으로 shop_search에 넘김
-    return render(request, "zerowaste/shop_search.html", {'info_list': search_list})
+    return render(request, "zerowaste/shop_search.html", {'info_list': search_list,
+                                                          'search_key': search_key,
+                                                          'categories': category_one,
+                                                          'subjects': subject,
+                                                          'facilities': facility})
 
 
 def info(request):
@@ -61,5 +107,11 @@ def info(request):
 def shop_detail(request, id):
     shop_detail = get_object_or_404(Shop, pk=id)
     # id와 똑같은 Nkreview 불러오기
+    form = CommentForm()
     review_detail = Nkreview.objects.filter(shop=id)
-    return render(request, 'zerowaste/shop_detail.html', {'shop_detail': shop_detail, 'review_detail':review_detail})
+    context = {
+        'shop_detail': shop_detail,
+        'form': form,
+        'review_detail': review_detail,
+    }
+    return render(request, 'zerowaste/shop_detail.html', context)
